c7cde324cbee2989f4e06b8d64d3868f5fa1c89c
JunwooDevelop <112684661+leejwjw@users.noreply.github.com>
2024-11-15 15:54:10 +0900

    최초 커밋

---
+++ src/main/java/com/pt/brewit/BrewitApplication.java
@@ -0,0 +1,13 @@
+package com.pt.brewit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewitApplication.class, args);
+	}
+
+}

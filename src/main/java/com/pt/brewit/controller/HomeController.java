763f7162fa07fe7b31b8e68e1a18a8db8bd05055
francha-web <58111863+francha-web@users.noreply.github.com>
2024-11-18 17:46:02 +0900

    login 화면과 controller추가
    login 화면과 controller추가

--- src/main/java/com/pt/brewit/controller/HomeController.java
+++ src/main/java/com/pt/brewit/controller/HomeController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 @Slf4j
 public class HomeController {
@@ -13,6 +14,9 @@ public class HomeController {
         log.info("home");
         return "main/mainPage";
     }
+
+
+
     @GetMapping("/admin")
     public String admin() {
         return "../static/admin/html/index";

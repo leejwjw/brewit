093ec24da19f79092923abed9e98f79c97de5861
JunwooDevelop <112684661+leejwjw@users.noreply.github.com>
2024-11-18 11:12:13 +0900

    기본 프레임 커밋
    기본 프레임 커밋

---
+++ src/main/java/com/pt/brewit/mapper/AdminMapper.java
@@ -0,0 +1,10 @@
+package com.pt.brewit.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+
+// 관리자 매퍼
+@Mapper
+public interface AdminMapper {
+}
---
+++ src/main/java/com/pt/brewit/service/AdminServiceImpl.java
@@ -0,0 +1,4 @@
+package com.pt.brewit.service;
+
+public class AdminServiceImpl {
+}

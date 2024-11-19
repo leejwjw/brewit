80677b9ef3197093787d4f6c4309113454128eda
JunwooDevelop <112684661+leejwjw@users.noreply.github.com>
2024-11-19 17:53:12 +0900

    productDTO 생성
    productDTO 생성

---
+++ src/main/java/com/pt/brewit/dto/ProductDTO.java
@@ -0,0 +1,24 @@
+package com.pt.brewit.dto;
+
+import lombok.Data;
+import java.util.Date;
+
+@Data
+public class ProductDTO {
+    private int product_id;
+    private int seller_id;
+    private String name;
+    private int category_id;
+    private int subcategory_id;
+    private String info;
+    private String nation;
+    private Integer weight;
+    private boolean is_caffeine;
+    private int stock;
+    private String attach_id;
+    private Integer hit;
+    private Date reg_date;
+    private Integer regular_price;
+    private Integer discount_price;
+    private Integer sale_price;
+}

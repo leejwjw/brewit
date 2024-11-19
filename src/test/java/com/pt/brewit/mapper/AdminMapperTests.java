7a8873374682306c1b509e8d3a550f7cae6bcba5
JunwooDevelop <112684661+leejwjw@users.noreply.github.com>
2024-11-19 09:49:27 +0900

    DB 테스트 코드 추가
    DB 테스트 코드 추가

---
+++ src/test/java/com/pt/brewit/mapper/AdminMapperTests.java
@@ -0,0 +1,35 @@
+package com.pt.brewit.mapper;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import lombok.extern.slf4j.Slf4j;
+import com.pt.brewit.dto.MemberDTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import java.util.List;
+
+@SpringBootTest
+@Slf4j
+public class AdminMapperTests {
+    @Autowired
+    private AdminMapper adminMapper;
+
+    @Test
+    public void test() {
+        log.info("adminMapper: {}", adminMapper);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<MemberDTO> list = adminMapper.selectAllMember();
+
+        // Java Stream
+        //list.forEach(boardDTO -> {log.info(boardDTO.toString());});
+        for (MemberDTO dto : list) {
+            log.info("dto: {}", dto.toString());
+        }
+    }
+
+}

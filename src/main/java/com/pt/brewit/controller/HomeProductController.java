bc67e9cdf5c7e15fd979ae55591f610bd179894c
Woo-Hyeok <eewasdf1230@gmail.com>
2024-11-21 18:20:10 +0900

    메인 페이지
    1. 메인 페이지 Fragment 화 완료
    2. 메인 카테고리 작업
       > 탭 클릭 시 해당 카테고리의 정보 표기 작업 완료
       > 링크 재 수정 필요

--- src/main/java/com/pt/brewit/controller/HomeProductController.java
+++ src/main/java/com/pt/brewit/controller/HomeProductController.java
@@ -4,6 +4,8 @@ import com.pt.brewit.dto.ProductDTO;
 import com.pt.brewit.service.MainProductService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,16 +20,16 @@ public class HomeProductController {
 
     //  인기 상품을 All / 차 / 커피 / 관련용품 등 눌러서 표기하는 프로그램 영역.
     @GetMapping("/products")
-    public List<ProductDTO> getProducts(@RequestParam String tab) {
-        switch (tab) {
-            case "all":
-                return mainProductService.getProductsForAll();
+    public List<ProductDTO> getProducts(@RequestParam String tab_id) {
+        switch (tab_id) {
+            case "best":
+                return mainProductService.getBestProducts();
             case "tea":
-                return mainProductService.getProductsForTea();
+                return mainProductService.getProductsByCategory(1);
             case "coffee":
-                return mainProductService.getProductsForCoffee();
-            case "tools":
-                return mainProductService.getProductsForAccessories();
+                return mainProductService.getProductsByCategory(2);
+            case "tool":
+                return mainProductService.getProductsByTool();
             default:
                 throw new IllegalArgumentException("탭 이름이 잘못되었습니다.");
         }

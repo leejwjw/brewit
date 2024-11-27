e6b539a011227cda8c6db2b1895eea3138a17183
Woo-Hyeok <eewasdf1230@gmail.com>
2024-11-27 16:03:48 +0900

    EventProductDTO 업뎃
    EventProductDTO 업뎃

--- src/main/java/com/pt/brewit/dto/EventProductDTO.java
+++ src/main/java/com/pt/brewit/dto/EventProductDTO.java
@@ -12,8 +12,11 @@ public class EventProductDTO {
     private String product_id;
     private int term;
     private int seller_id;
+    private String subtitle;
     private String event_name;
     private String event_info;
+    private String attach_info;
+    private String attach_origin_info;
     private String attach_name;
     private String attach_origin_name;
     private int discount;

f267186f93e5347e06cb858b781c6fa6fd75448d
Woo-Hyeok <eewasdf1230@gmail.com>
2024-11-28 15:05:35 +0900

    AttachmentDTO 생성
    AttachmentDTO 생성

---
+++ src/main/java/com/pt/brewit/dto/AttachmentDTO.java
@@ -0,0 +1,13 @@
+package com.pt.brewit.dto;
+
+import java.util.Date;
+
+public class AttachmentDTO {
+    private int attach_id;
+    private int member_id;
+    private String name;
+    private String origin_name;
+    private String type;
+    private String mime;
+    private Date reg_date;
+}
